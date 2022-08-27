24c0219c656f54ab31b334ec9985afda2d73f6dd
sadykov0404 <sadykovmustafo0404@gmail.com>
2022-08-28 01:08:55 +0500

    nofinal

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,265 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe_game/ui/color.dart';
+import 'package:tic_tac_toe_game/utils/game_model.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: GameScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GameScreen> createState() => _GameScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GameScreenState extends State<GameScreen> {
+  String lastValue = "X";
+  bool gameOver = false;
+  int turn = 0;
+  String result = "";
+  List<int> scoreboard = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+  ]; //rows[1,2,3], cols[1,2,3],diagonal[1,2,3,]
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Game game = Game();
+
+  @override
+  void initState() {
+    super.initState();
+    game.board = Game.initGameBoard();
+    print(result);
+    resultModal();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double boardWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      // backgroundColor: MainColor.primaryColor,
+      backgroundColor: Color(0xff192A32),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 50,
+            width: 120,
+            padding: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8.0),
+              color: Color(0xff1F353F),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 0,
+                    color: Color(0xff102129),
+                    offset: Offset(0, 5))
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            child: InkWell(
+              onTap: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 30,
+                    child: lastValue == "X"
+                        ? SvgPicture.asset(
+                            "assets/images/x_game.svg",
+                            color: Color(0xff31C4BE),
+                          )
+                        : SvgPicture.asset(
+                            "assets/images/o_game.svg",
+                            color: Color(0xffF2B237),
+                          ),
+                  ),
+                  Text(
+                    "Turn".toUpperCase(),
+                    style: TextStyle(
+                        color: Color(0xffA8BEC9),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            width: boardWidth,
+            height: boardWidth,
+            child: GridView.count(
+              crossAxisCount: Game.boardLength ~/ 3, //method
+              padding: EdgeInsets.all(16.0),
+              crossAxisSpacing: 20.0,
+              mainAxisSpacing: 20.0,
+              children: List.generate(Game.boardLength, (index) {
+                return InkWell(
+                  onTap: gameOver
+                      ? () => resultModal()
+                      : () {
+                          if (game.board![index] == "") {
+                            setState(() {
+                              game.board![index] = lastValue;
+                              turn++;
+                              gameOver = game.winnerCheck(
+                                  lastValue, index, scoreboard, 3);
+
+                              if (gameOver) {
+                                result = "${lastValue} is the Winner";
+                              } else if (!gameOver && turn == 9) {
+                                result = "It's a Drawl!";
+                                gameOver = true;
+                              }
+                              if (lastValue == "X")
+                                lastValue = "O";
+                              else
+                                lastValue = "X";
+                            });
+                          }
+                        },
+                  child: Container(
+                    width: Game.blocSize,
+                    height: Game.blocSize,
+                    decoration: BoxDecoration(
+                        // color: MainColor.secondaryColor,
+                        color: Color(0xff1F353F),
+                        borderRadius: BorderRadius.circular(16.0),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 0,
+                              color: Color(0xff102129),
+                              offset: Offset(0, 10))
+                        ]),
+                    child: Center(
+                        // child: Text(
+                        //   game.board![index],
+                        //   style: TextStyle(
+                        //     color: game.board![index] == "X"
+                        //         ? Colors.blue
+                        //         : Colors.pink,
+                        //     fontSize: 64.0,
+                        //   ),
+                        // ),
+                        child: status(index)),
+                  ),
+                );
+              }),
+            ),
+          ),
+          SizedBox(
+            height: 25.0,
+          ),
+          Text(
+            result,
+            style: TextStyle(color: Colors.white, fontSize: 54.0),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              resultModal();
+
+              setState(() {
+                game.board = Game.initGameBoard();
+                lastValue = "X";
+                gameOver = false;
+                turn = 0;
+                result = "";
+                scoreboard = [
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                ];
+              });
+            },
+            icon: Icon(Icons.replay),
+            label: Text("Repeat the Game"),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  status(index) {
+    if (game.board![index] == "X") {
+      return SizedBox(
+        width: 70,
+        child: SvgPicture.asset(
+          "assets/images/x_game.svg",
+          color: Color(0xff31C4BE),
+        ),
+      );
+    } else if (game.board![index] == "O") {
+      return SizedBox(
+        width: 70,
+        child: SvgPicture.asset(
+          "assets/images/o_game.svg",
+          color: Color(0xffF2B237),
+        ),
+      );
+    }
+  }
+
+  // @override
+  void resultModal() {
+    result == ""
+        ? null
+        : showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                backgroundColor: Colors.transparent,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16.0),
+                    color: Color(0xff1F353F),
+                  ),
+                  child: Column(
+                    children: [
+                      // IconButton(
+                      //   onPressed: () {},
+                      //   icon: SvgPicture.asset(
+                      //     "assets/images/x_game.svg",
+                      //   ),
+                      // )
+                    ],
+                  ),
+                ),
+              );
+            });
   }
 }
